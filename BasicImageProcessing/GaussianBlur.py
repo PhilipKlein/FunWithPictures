6c3655c341ac1f28591f7e6ef0696c761290ae84
Philip <philipkln.91@gmail.com>
2017-08-12 19:39:25 +0200

    Added Blur File

---
+++ BasicImageProcessing/GaussianBlur.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy import misc
+
+
+def load_image():
+    pass
+
+
+def apply_gaussian_blur(img, sigma=1):
+    # calculate the gaussian mask
+    # apply 
+    pass
+
+def save_image():
+    pass
+
+if __name__ == "__main__":
+    load_image()
